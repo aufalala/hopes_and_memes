1fbbe4499dbd6b009da302ff37839d6b4652188d
Aufa <aufa_romeo@live.com>
2025-08-31 23:53:19 +0800

    feat: connected airtable

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
+const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
+const AIRTABLE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/testTable`;
+
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -12,4 +19,19 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
 
+app.get('/', (req, res) => {
+  res.send('Server is working!');
+});
 
+//initial connection test
+async function getRecords() {
+  const response = await fetch(AIRTABLE_URL, {
+    headers: {
+      Authorization: `Bearer ${AIRTABLE_TOKEN}`
+    }
+  });
+
+  const data = await response.json();
+  console.log(JSON.stringify(data, null, 2));
+}
+getRecords();
