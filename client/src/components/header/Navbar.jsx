46af6eb0467ebeb33b6983c2597e8e25b7798879
Aufa <aufa_romeo@live.com>
2025-09-08 23:01:05 +0800

    [CLIENT] test: moved navbar test li to more

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -1,12 +1,8 @@
 import { Link } from "react-router-dom";
-import { fetchTestImage, fetchTestImageProtected } from "../../utils/airtableAPI";
-import { fetchRandomMeme } from "../../utils/memeAPI";
-import { useClerkAuthFetch } from "../../hooks/useClerkAuthFetch";
 import styles from "./_Navbar.module.css"
 
 function Navbar() {
   
-  const { fetchWithAuth } = useClerkAuthFetch();
   const links = [
     {
       path: "/",
@@ -37,50 +33,6 @@ function Navbar() {
           )
         })}
 
-        <li>
-          <div
-            onClick={async () => {
-              try {
-                const meme = await fetchTestImage(fetchWithAuth);
-                console.log(meme);
-              } catch (err) {
-                console.error("Failed to fetch test image:", err.message);
-              }
-            }}
-          >
-            get unprotected image in console
-          </div>
-        </li>
-
-        <li>
-          <div
-            onClick={async () => {
-              try {
-                const meme = await fetchRandomMeme();
-                console.log(meme);
-              } catch (err) {
-                console.error("Failed to fetch meme:", err.message);
-              }
-            }}
-          >
-            get random meme in console
-          </div>
-        </li>
-
-        <li>
-          <div
-            onClick={async () => {
-              try {
-                const meme = await fetchTestImageProtected(fetchWithAuth);
-                console.log(meme);
-              } catch (err) {
-                console.error("Failed to fetch test image protected:", err.message);
-              }
-            }}
-          >
-            get protected test image in console
-          </div>
-        </li>
       </ul>
     </nav>
   );
--- client/src/pages/More.jsx
+++ client/src/pages/More.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { fetchTestImage, fetchTestImageProtected } from "../utils/airtableAPI";
+import { fetchRandomMeme } from "../utils/memeAPI";
+
+import { useClerkAuthFetch } from "../hooks/useClerkAuthFetch";
 
 function More() {
+  
+  const { fetchWithAuth } = useClerkAuthFetch();
+
   return (
     <>
-    
+    <li>
+          <div
+            onClick={async () => {
+              try {
+                const meme = await fetchTestImage(fetchWithAuth);
+                console.log(meme);
+              } catch (err) {
+                console.error("Failed to fetch test image:", err.message);
+              }
+            }}
+          >
+            get unprotected image in console
+          </div>
+        </li>
+
+        <li>
+          <div
+            onClick={async () => {
+              try {
+                const meme = await fetchRandomMeme();
+                console.log(meme);
+              } catch (err) {
+                console.error("Failed to fetch meme:", err.message);
+              }
+            }}
+          >
+            get random meme in console
+          </div>
+        </li>
+
+        <li>
+          <div
+            onClick={async () => {
+              try {
+                const meme = await fetchTestImageProtected(fetchWithAuth);
+                console.log(meme);
+              } catch (err) {
+                console.error("Failed to fetch test image protected:", err.message);
+              }
+            }}
+          >
+            get protected test image in console
+          </div>
+        </li>
     </>
   )
 }
