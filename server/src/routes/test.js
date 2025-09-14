61234c57adc329a0e6c2c84eab7749f3306d0875
Aufa <aufa_romeo@live.com>
2025-09-15 01:53:12 +0800

    [SERVER] fix: reddis for test meme-count

--- server/src/routes/test.js
+++ server/src/routes/test.js
@@ -53,27 +53,30 @@ router.get("/testImage/protected", requireAuth(), async (req, res) => {
 router.get("/meme-count", async (req, res) => {
   try {
     const result = await getUnratedMemeCount();
-    if (result.status === "success") {
-      res.json(result);
-      if (result.count <= 5) {
-        const existingJobs = await queues.getTenMemesQueue.getWaiting();
-        console.log(existingJobs);
-        const alreadyQueued = existingJobs.length > 0;
-
-        if (!alreadyQueued) {
-          await queues.getTenMemesQueue.add({}, { removeOnComplete: true, removeOnFail: true });
-          console.log('getTenMemesQueue job added to queue');
-        } else {
-          console.log('getTenMemesQueue job already queued');
-        }
-      }
-
-    } else {
+    if (result.status !== "success") {
       return res.status(500).json(result);
     }
+    
+    if (result.count <= 5) {
+      const existingJobs = await queues.getTenMemesQueue.getWaiting();
+      console.log(existingJobs);
+      const alreadyQueued = existingJobs.length > 0;
+
+      if (!alreadyQueued) {
+        await queues.getTenMemesQueue.add({}, { removeOnComplete: true, removeOnFail: true });
+        console.log('getTenMemesQueue job added to queue');
+      } else {
+        console.log('getTenMemesQueue job already queued');
+      }
+    }
+
+    res.json(result);
+
   } catch (e) {
     console.error(e);
-    return res.status(500).json(e.message);
+    if (!res.headersSent) {
+      return res.status(500).json({ error: e.message });
+    }
   }
 });
 
