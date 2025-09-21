a64358289dcb38a5e1f9c39067d64865737c9b46
Aufa <aufa_romeo@live.com>
2025-09-21 18:20:29 +0800

    [SERVER] FEAT: table using ID, dotenv

--- server/src/config.js
+++ server/src/config.js
@@ -6,6 +6,12 @@ dotenv.config();
 export const AIRTABLE_URL = process.env.AIRTABLE_URL;
 export const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
 
+export const AIRTABLE_T_UNRATED_MEMES = process.env.AIRTABLE_T_UNRATED_MEMES;
+export const AIRTABLE_T_RATED_MEMES = process.env.AIRTABLE_T_RATED_MEMES;
+export const AIRTABLE_T_ALL_USERS = process.env.AIRTABLE_T_ALL_USERS;
+export const AIRTABLE_T_MEME_RATINGS = process.env.AIRTABLE_T_MEME_RATINGS;
+
+
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
