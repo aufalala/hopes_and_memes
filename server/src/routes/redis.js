bbe2afc9c0c3f37e5efda24dd11e0df8b536f800
Aufa <aufa_romeo@live.com>
2025-09-27 21:10:56 +0800

    [SERVER] FEAT: route for rating rated meme

--- server/src/routes/redis.js
+++ server/src/routes/redis.js
@@ -2,6 +2,7 @@ import express from "express";
 import { requireAuth, getAuth } from "@clerk/express";
 
 import { contRatingUnratedMemes } from "../controllers/contRatingUnratedMeme.js";
+import { orcRateRatedMeme } from "../orchestrators/orcRateRatedMeme.js";
 import { getUnratedMemesFromCache } from "../services/redisAPI.js";
 
 import getTimestamp from "../utils/utTimestamp.js";
@@ -35,6 +36,30 @@ router.post("/unrated-meme-rating", requireAuth(), async (req, res) => {
 
   try {
     const result = await contRatingUnratedMemes(sourceData, req)
+
+    if (result.status === "success") {
+      return res.json(result);
+    } else {
+      res.status(500).json(result);
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json(e.message);
+  }
+});
+
+router.post("/rated-meme-rating", requireAuth(), async (req, res) => {
+  const sourceData = `${req.method} ${req.originalUrl} from ${req.ip}`;
+  console.log(`[${getTimestamp()}] CLIENT REACHED: ${sourceData}`);
+
+  const {userId} = getAuth(req);
+  if (!userId) {
+      return res.status(401).json({ error: "Not authenticated" });
+    }
+
+  try {
+    const result = await orcRateRatedMeme({sourceData, req})
+    console.log("sadsadas")
 
     if (result.status === "success") {
       return res.json(result);
