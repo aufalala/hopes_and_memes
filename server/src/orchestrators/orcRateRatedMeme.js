a24b6d8a4db6e41b0b1f955ec35510a4ca0d9d7d
Aufa <aufa_romeo@live.com>
2025-09-28 23:52:47 +0800

    [SERVER] REFAC & FEAT: update meme rating if user prev rated. add points refac.

--- server/src/orchestrators/orcRateRatedMeme.js
+++ server/src/orchestrators/orcRateRatedMeme.js
@@ -1,11 +1,11 @@
 import { getAuth } from "@clerk/express";
-import redisConnection from "../redis/connection.js";
 
-import { AIRTABLE_T_ALL_USERS, AIRTABLE_T_MEME_RATINGS } from "../config.js";
+import { AIRTABLE_T_MEME_RATINGS } from "../config.js";
 import { getRecordsFromAirtable, postToAirtable, updateAirtableRecord } from "../services/airtableAPI.js";
-import { updateCacheHash } from "../services/redisAPI.js";
+import { getHashValuesFromCache, updateCacheHash } from "../services/redisAPI.js";
 
 import getTimestamp from "../utils/utTimestamp.js";
+import { orcAddPoints } from "./orcAddPoints.js";
 
 export async function orcRateRatedMeme({sourceData, postParams = {}, req}) {
   console.log(`[${getTimestamp()}] TRYING: orcRateRatedMeme from ${sourceData}`);
@@ -19,6 +19,7 @@ export async function orcRateRatedMeme({sourceData, postParams = {}, req}) {
   postParams.created_at ??= String(Date.now());
 
   let ratings;
+  let userRatingId;
   let flagRatingsExist = false;
   let flagUserRatingExist = false;
 
@@ -28,6 +29,7 @@ export async function orcRateRatedMeme({sourceData, postParams = {}, req}) {
     if (ratings.status === "success" && ratings.records.length > 0) {
       flagRatingsExist = true;
     }
+
   } catch (e) {
     console.error("orcRateUnratedMeme: getRecordsFromAirtable FAILED:", e);
     throw e;
@@ -40,12 +42,80 @@ export async function orcRateRatedMeme({sourceData, postParams = {}, req}) {
     );
   }
 
-  //222// UPDATE MEME RATING IF ALREADY RATED
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //222// IF ALREADY RATED BY USER, UPDATE MEME RATING IN AIRTABLE AND CACHE 
   if (flagRatingsExist && flagUserRatingExist) {
-    //DO PATCH RATINGS RECORDS
+
+    //222// GET USER RATING IN AIRTABLE
+    try {
+      const result = await getRecordsFromAirtable({
+        sourceData,
+        table: AIRTABLE_T_MEME_RATINGS,
+        filterParams: {clerk_user_id: postParams.clerk_user_id, postLink: postParams.postLink},
+        fullData: true,
+      })
+      if (result.status === "success" && result.records.length > 0) {
+        userRatingId = result.records[0].id;
+      }
+
+    } catch (e) {
+      console.error("orcRateRatedMeme: getRecordsFromAirtable FAILED:", e);
+      throw e;
+    }
+    
+    //222// UPDATE USER RATING IN AIRTABLE
+    try {
+      await updateAirtableRecord({
+        table: AIRTABLE_T_MEME_RATINGS,
+        recordId: userRatingId,
+        fields: { rating: String(postParams.rating) }
+      });
+      console.log("User rating updated to airtable successfully");
+
+    } catch (e) {
+      console.error("orcRateRatedMeme: updateAirtableRecord FAILED:", e);
+      throw e;
+    }
+
+
+    let userRatingRecord;
+    //222// GET PREVIOUS USER RATING IN CACHE
+    try {
+      const result = await getHashValuesFromCache({ keyPrefix: `userRating:${userId}`, valueParam: postParams.postLink});
+      if (result.status === "success") {
+        userRatingRecord = result.records;
+      }
+
+    } catch (e) {
+      console.error("orcRateRatedMeme: getHashValuesFromCache FAILED:", e);
+      throw e;
+    }
+    
+    //222// AMEND RATING IN OBJECT
+    userRatingRecord.rating = postParams.rating;
+    
+    //222// UPDATE USER RATING IN CACHE
+    try {
+      const result = await updateCacheHash({
+        keyPrefix: "userRating",
+        record: userRatingRecord,
+        groupKeyField: "clerk_user_id",
+        identifierField: "postLink",
+      });
+      console.log("Rating added to cache successfully");
+
+    } catch (e) {
+      console.error("orcRateRatedMeme: updateCacheHash FAILED:", e);
+      throw e;
+    }
+
+    return {status: "success", message: "Updated rating"}
   }
 
-  //222// POST RATING TO AIRTABLE, ADD RATING TO CACHE, ADD POINTS TO USER AT, ADD POINTS TO CACHE
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //222// IF NOT RATED BY USER, POST RATING TO AIRTABLE, ADD RATING TO CACHE, ADD POINTS TO USER AT, ADD POINTS TO CACHE
   if (flagRatingsExist && !flagUserRatingExist) {
 
     
@@ -57,9 +127,6 @@ export async function orcRateRatedMeme({sourceData, postParams = {}, req}) {
     //222// POST RATING TO AIRTABLE 
     try {
       const result = await postToAirtable({sourceData, table: AIRTABLE_T_MEME_RATINGS, postParams}); 
-        // if (result.status === "success") {
-        // }
-        
       console.log("Rating added to airtable successfully");
 
     } catch (e) {
@@ -82,69 +149,16 @@ export async function orcRateRatedMeme({sourceData, postParams = {}, req}) {
       throw e;
     }
 
-
-
-    //ADD POINTS HERE
-    //
-    // 00000000000000000000000000000000 CAN BE ORC 
-    //
-
-    const pointsToAdd = 2;
-    const userKey = `users:${postParams.clerk_user_id}`
-    let userRecordId;
-    let userPoints;
-
-    
-    //222// GET USER RECORD ID AND POINTS FROM AIRTABLE
+    //222// ORC ADD POINTS
     try {
-      const result = await getRecordsFromAirtable({
-        sourceData,
-        table: AIRTABLE_T_ALL_USERS,
-        filterParams: {clerk_user_id: postParams.clerk_user_id},
-        fullData: true,
-      })
-      if (result.status === "success") {
-        userRecordId = result.records[0].id;
-        userPoints = result.records[0].fields.points;
-      }
-    } catch (e) {
-      console.error("orcRateRatedMeme: getRecordsFromAirtable FAILED:", e);
-      throw e;
-    }
-
-    userPoints = (Number(userPoints) || 0) + pointsToAdd;
-
-    //222// UPDATE USER POINTS IN AIRTABLE
-    try {
-      await updateAirtableRecord({
-        table: AIRTABLE_T_ALL_USERS,
-        recordId: userRecordId,
-        fields: { points: String(userPoints) }
-      });
-      
-      console.log("User points updated to airtable successfully");
-    } catch (e) {
-      console.error("orcRateRatedMeme: updateAirtableRecord FAILED:", e);
-      throw e;
-    }
-
-    //222// ADD POINTS TO CACHE
-    try {
-      const cacheUserData = await redisConnection.get(userKey);
-      if (!cacheUserData) throw new Error("User not found in Redis");
-
-      const userData = JSON.parse(cacheUserData);
-      userData.points = userPoints;
-
-      await redisConnection.set(userKey, JSON.stringify(userData));
-
-      console.log("User points updated to cache successfully:", userData.points);
-      return {status: "success"}
+      const result = await orcAddPoints({sourceData, postParams, pointsToAdd: 2})
+      console.log("orcRateRatedMeme: SUCCESS");
       
     } catch (e) {
-      console.error("orcRateRatedMeme: addPointsToCache FAILED:", e);
+      console.error("orcRateRatedMeme: updateCacheHash FAILED:", e);
       throw e;
     }
 
+    return {status: "success", message: "Rated a rated meme"}
   }
 }
