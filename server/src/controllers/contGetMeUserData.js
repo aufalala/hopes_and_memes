5ac2460d54436949de72a33c4c06e57978154f36
Aufa <aufa_romeo@live.com>
2025-10-02 21:29:09 +0800

    [SERVER] FEAT: new user data now added to cache

--- server/src/controllers/contGetMeUserData.js
+++ server/src/controllers/contGetMeUserData.js
@@ -1,6 +1,7 @@
 import { getAuth, clerkClient} from "@clerk/express";
 import { AIRTABLE_T_ALL_USERS } from "../config.js";
 import { getRecordsFromAirtable, postUser } from "../services/airtableAPI.js";
+import { setRedisCache } from "../services/redisAPI.js";
 
 import getTimestamp from "../utils/utTimestamp.js";
 
@@ -19,16 +20,26 @@ export async function contGetMeUserData({sourceData, req, res}) {
 
   } else if (result.status === "success" && result.records.length === 0) {
 
+
+      const userData = {clerk_user_id: userId, username: username, created_at: String(createdAt), num_memes_rated: "0", points: "0"}
+      try {
+        const result3 = await setRedisCache(`users:${userId}`, userData)
+      } catch (e) {
+          console.error("Error posting user:", e.message);
+          return res.status(500).json({ error: "Failed to post user" });
+      }
+
       try {   
         const result2 = await postUser(sourceData, userId, username, createdAt)
         if (result2.status === "success") {
           return (result2);
         }
     
-      } catch (error) {
-          console.error("Error posting user:", error.message);
+      } catch (e) {
+          console.error("Error posting user:", e.message);
           return res.status(500).json({ error: "Failed to post user" });
-        }
+      }
+
         
   } else {
     res.status(500).json(result);
